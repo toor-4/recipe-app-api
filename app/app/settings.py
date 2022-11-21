7b30c8711d16dc69cfad407a12ab4066a247dc6a
alfred <frealchar@gmail.com>
2022-11-21 15:15:51 +0800

    deleted blog app

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # myapp
-    'blog.apps.BlogConfig'
+
 ]
 
 MIDDLEWARE = [
